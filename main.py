4387ab047ae13455050a0b58aabe3e42c9b4b5a7
Ahmed Saleh <ahmed.ib3110@gmail.com>
2023-09-24 03:57:53 +0300

    General improvements

--- main.py
+++ main.py
@@ -13,33 +13,33 @@ screen.addshape(blank_states_img)
 turtle.shape(blank_states_img)
 
 # Read the CSV file
-states_source = pandas.read_csv("data/50_states.csv")
+state_source = pandas.read_csv("data/50_states.csv")
 
-states_names = states_source.state.to_list()  # Create a list of states names
-states_xcor = states_source.x.to_list()  # Create a list of 'X' coordinates
-states_ycor = states_source.y.to_list()  # Create a list of 'Y' coordinates
-states_guessed = []  # Create an empty list that will store the names of states guessed correctly
+state_names = state_source.state.to_list()  # Create a list of states names
+state_xcor = state_source.x.to_list()  # Create a list of 'X' coordinates
+state_ycor = state_source.y.to_list()  # Create a list of 'Y' coordinates
+state_guessed = []  # Create an empty list that will store the names of states guessed correctly
 
-states_correct = 0  # Track the number of states guessed correctly
-answer_state_title = "Guess the State"  # Create a string for the title of the prompt window
+st_ans_correct = 0  # Track the number of states guessed correctly
+st_ans_title = "Guess the State"  # Create a string for the title of the prompt window
 
 
 # Run the game until '50' states are guessed correctly
-while states_correct < 50:
+while st_ans_correct < 50:
 
-    if states_correct > 0:
-        answer_state_title = f"{states_correct}/50 States Correct"
-    answer_state = screen.textinput(title=answer_state_title, prompt="Enter a state name:").lower().title()
+    if st_ans_correct > 0:
+        st_ans_title = f"{st_ans_correct}/50 States Correct"
+    st_ans_input = screen.textinput(title=st_ans_title, prompt="Enter a state name:").title()
 
     # Check if the answer is inside the states names list and NOT inside the states already guessed by the user
-    if answer_state in states_names and answer_state not in states_guessed:
-        states_index = states_names.index(answer_state)
+    if st_ans_input in state_names and st_ans_input not in state_guessed:
+        st_index = state_names.index(st_ans_input)
         # Write the name of the state
-        writer.write_at(text=states_names[states_index],
-                        location=(states_xcor[states_index], states_ycor[states_index]))
+        writer.write_at(text=state_names[st_index],
+                        location=(state_xcor[st_index], state_ycor[st_index]))
         # Add the state name to the list of 'already guessed' states
-        states_guessed.append(states_names[states_index])
+        state_guessed.append(state_names[st_index])
 
-        states_correct += 1
+        st_ans_correct += 1
 
 screen.exitonclick()

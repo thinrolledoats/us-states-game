9a7f1b5e88fcf08b4e559293d4f7deb9d5686aae
Ahmed Saleh <ahmed.ib3110@gmail.com>
2023-09-24 04:20:33 +0300

    General improvements

--- main.py
+++ main.py
@@ -21,12 +21,13 @@ state_ycor = state_source.y.to_list()  # Create a list of 'Y' coordinates
 state_gsd = []  # Create an empty list that will store the names of states guessed correctly
 state_rem = []  # Create an empty list that will store the remaining states (NOT guessed)
 
+st_ans_input = None
 st_ans_correct = 0  # Track the number of states guessed correctly
 st_ans_title = "Guess the State"  # Create a string for the title of the prompt window
 
 
 # Run the game until '50' states are guessed correctly
-while st_ans_correct < 50:
+while st_ans_correct < len(state_names):
 
     if st_ans_correct > 0:
         st_ans_title = f"{st_ans_correct}/50 States Correct"
@@ -42,15 +43,16 @@ while st_ans_correct < 50:
         state_gsd.append(state_names[st_index])
 
         st_ans_correct += 1
+
     elif st_ans_input == "Exit":
         for state in state_names:
             if state not in state_gsd:
                 state_rem.append(state)
-        state_exp_dict = {
-            'state': state_rem
-        }
-        state_exp_df = pandas.DataFrame(state_exp_dict)
+        state_rem_dict = {'remaining': state_rem}
+        # Create a DataFrame from the dictionary of remaining states and export it to .CSV
+        state_exp_df = pandas.DataFrame(state_rem_dict)
         state_exp_df.to_csv("export/state_export.csv")
         break
 
-screen.exitonclick()
+if st_ans_input != "Exit":
+    screen.exitonclick()
